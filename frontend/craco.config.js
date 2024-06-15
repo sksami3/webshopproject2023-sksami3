a5d442f05d1855f286004a07bc8374787ae3b66f
sksami3 <sksami4456@gmail.com>
2024-06-16 00:04:51 +0300

    chore: added craco

---
+++ frontend/craco.config.js
@@ -0,0 +1,21 @@
+// craco.config.js
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+        },
+      },
+      plugins: [
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+      ],
+    },
+  },
+};
